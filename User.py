8aba5df73fd25269acab6cc0b100b59d64207d61
nbehmane <nbehmane@calpoly.edu>
2021-08-05 14:21:25 -0700

    Changed Individual --> User. Boy I feel dumb not thinking about 'User' first.

--- User.py
+++ User.py
@@ -1,6 +1,8 @@
 import pymongo
 
-class Individual():
+class User():
     """Someone that can be assigned chores/tasks."""
     def __init__(name=None):
+        """Initialize a user."""
         self.name = name
+    
