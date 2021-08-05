f843d3e8a706f5c367d2585fe0f57ae93552e6a1
nbehmane <nbehmane@calpoly.edu>
2021-08-05 14:31:26 -0700

    Added skeleton user repo.

--- User.py
+++ User.py
@@ -1,9 +1,27 @@
-import pymongo
+from pymongo import MongoClient
 
 class UserRepo():
     """The user repository to interface with MongoDB."""
-    def __init__():
+    def __init__(self, mongo_url):
+        """Initializes the interface to the MongoDB"""
+        self.client = MongoClient(mongo_url)
+        self.database = self.client['Users']
+        self.collection = self.database['UserInformation']
+    
+    def create(self, user):
+        """Create a User in the database given a user object."""
         pass
+    def  read(self, user=None):
+        """Read the database for users, or a specific user 
+            given a User object"""
+        pass
+    def update(self, user):
+        """Updates a user in the database given a User object."""
+        pass
+    def delete(self, user):
+        """Delete a user from the database given a User object."""
+        pass
+    
 
 class User():
     """Someone that can be assigned chores/tasks."""
