22077094bbf327a6b49ec9d1ca4954d6bd6271a1
nbehmane <nbehmane@calpoly.edu>
2021-08-05 14:23:21 -0700

    Added a repo class.

--- User.py
+++ User.py
@@ -1,8 +1,16 @@
 import pymongo
 
+class UserRepo():
+    """The user repository to interface with MongoDB."""
+    def __init__():
+        pass
+
 class User():
     """Someone that can be assigned chores/tasks."""
-    def __init__(name=None):
+    def __init__(name=None, tasks=[]):
         """Initialize a user."""
         self.name = name
+        self.tasks = tasks
+
+
     
